2908b8f64ffa502ea2463a4d5c2c1133889bcd6f
júlio vaz <julio.oliveiravaz0@gmail.com>
2019-11-18 22:31:43 -0200

    consumindo dados da pokeapi via fetch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+class App extends React.Component{
+
+  state = {
+    pokemons: []
+  }
+
+  componentDidMount = async () => {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon-species/?limit=20&offset=0`);
+    const pokemons = await response.json();
+    this.setState({pokemons: pokemons.results});
+  }
+
+  render(){
+    const { pokemons } = this.state;
+    console.log(pokemons);
+    return (
+      <div className="App">
+        <ul>
+          {
+            pokemons.map(pokemon => (
+              <li>{pokemon.name}</li>
+            ))
+          }
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
