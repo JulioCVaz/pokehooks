a95caa0da5efdb91d1b31981345cdbfe476b1d34
júlio vaz <julio.oliveiravaz0@gmail.com>
2019-11-18 23:51:07 -0200

    pokedex inicial, consumindo e renderizando dados da pokeapi

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './styles/style.css';
 
 class App extends React.Component{
 
   state = {
-    pokemons: []
+    pokemons: [],
+    pokemonFinded: [],
+    selectedPokemon: ''
   }
 
   componentDidMount = async () => {
@@ -12,18 +15,47 @@ class App extends React.Component{
     this.setState({pokemons: pokemons.results});
   }
 
+  handleSearchPokemon = async (pokemonName) => {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+    const pokemonFinded = await response.json();
+    this.setState({pokemonFinded: [ pokemonFinded ]});
+  }
+
+  activeItem = (e) => {
+    let pokemonName = e.target.innerText;
+    this.setState({selectedPokemon: pokemonName}, () => this.handleSearchPokemon(pokemonName));
+  } 
+
   render(){
-    const { pokemons } = this.state;
-    console.log(pokemons);
+    const { pokemons, selectedPokemon, pokemonFinded } = this.state;
+    console.log(pokemonFinded.length);
     return (
-      <div className="App">
-        <ul>
-          {
-            pokemons.map(pokemon => (
-              <li>{pokemon.name}</li>
-            ))
-          }
-        </ul>
+      <div>
+        <div>
+          <h1>{selectedPokemon}</h1>
+          <nav>
+            <ul style={{overflow:"hidden", overflowY :"scroll", height:"200px", width: "150px"}}>
+              {
+                pokemons.map((pokemon, index) => (
+                  <li onClick={(e) => this.activeItem(e)} key={index}>
+                    <span style={{display: "none"}}>{pokemon.url}</span>
+                    {pokemon.name}
+                  </li>
+                ))
+              }
+              </ul>
+            </nav>
+          </div>
+          <div>
+            {
+              (pokemonFinded.length > 0) && pokemonFinded.map(pokemon => (
+                <div key={pokemon.id}>
+                  <h3>{pokemon.name}</h3>
+                  <img src={pokemon.sprites.front_default} />
+                </div>
+              ))
+            }
+          </div>
       </div>
     );
   }
