d74ca38d88048a10ceb17732ffc0d956a06f201a
JulioCVaz <julio.oliveiravaz0@gmail.com>
2019-11-22 19:33:33 -0300

    migrado componente App para hooks

--- src/App.js
+++ src/App.js
@@ -1,62 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './styles/fonts.css';
 import './styles/style.css';
 import PokemonList from './components/PokemonList';
 import PokemonView from './components/PokemonView';
 
-class App extends React.Component{
+const App = () => {
 
-  state = {
-    pokemons: [],
-    qtdPokemons: 0,
-    pokemonFound: [],
-    selectedPokemon: ''
-  }
+  const [pokemons, setPokemons] = useState([]);
+  const [pokemonFound, setPokemonFound] = useState([]);
+  const [activeIndex, setActiveIndex] = useState(0);
 
-  componentDidMount = async () => {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon`);
-    const pokemons = await response.json();
-    this.setState({pokemons: pokemons.results, qtdPokemons: pokemons.count});
-  }
 
-  handleSearchPokemon = async (pokemonName) => {
+  useEffect(() => {
+    async function fetchData(){
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon`);
+      const pokemons = await response.json();
+      setPokemons(pokemons.results);
+    }
+    fetchData();
+  }, []);
+
+
+  async function handleSearchPokemon(pokemonName){
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
     const pokemonFound = await response.json();
+    setPokemonFound(pokemonFound);
 
-    this.setState({pokemonFound: [pokemonFound]})
+    // this.setState({pokemonFound: [pokemonFound]})
   }
 
-  activeItem = (e, index) => {
+  function activeItem(e, index){
     let pokemonName = e.target.innerText;
-    this.setState({activeIndex: index}, () => this.handleSearchPokemon(pokemonName));
+    setActiveIndex(index);
+    setTimeout(() => {
+      handleSearchPokemon(pokemonName)
+    }, 100);
   } 
 
-  render(){
-    const { pokemons, pokemonFound, activeIndex } = this.state;
-    return (
-      <div className="container">
-        <div className="pokedex-moldure">
-          <div className="pokedex-header">
-            <div className="circle-main"></div>
-            <div className="circle-one"></div>
-            <div className="circle-two"></div>
-            <div className="circle-tree"></div>
-          </div>
-          <div className="pokedex-content">
-            <PokemonList pokemons={pokemons} activeItem={this.activeItem} activeIndex={activeIndex}/>
-            <PokemonView pokemonFound={pokemonFound} />
-          </div>
-          <div className="pokedex-controls">
-            <div className="plus-direction">+</div>
-            <div className="wrapper-commands">
-              <div className="command-one"></div>
-              <div className="command-two"></div>
-            </div>
+  return (
+    <div className="container">
+      <div className="pokedex-moldure">
+        <div className="pokedex-header">
+          <div className="circle-main"></div>
+          <div className="circle-one"></div>
+          <div className="circle-two"></div>
+          <div className="circle-tree"></div>
+        </div>
+        <div className="pokedex-content">
+          <PokemonList pokemons={pokemons} activeItem={activeItem} activeIndex={activeIndex}/>
+          <PokemonView pokemonFound={pokemonFound} />
+        </div>
+        <div className="pokedex-controls">
+          <div className="plus-direction">+</div>
+          <div className="wrapper-commands">
+            <div className="command-one"></div>
+            <div className="command-two"></div>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
 
 export default App;
